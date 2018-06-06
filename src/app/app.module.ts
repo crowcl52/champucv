6f7cbb157536cab9dba8b93f840a2cc358e55821
eduardo SANCHEZ <eduardo.sanchez@aori.mx>
2018-06-05 22:11:59 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/view/landing/landing.component';
+import { AboutComponent } from './components/landing/about/about.component';
+import { MenuComponent } from './components/landing/menu/menu.component';
+import { SkillsComponent } from './components/landing/skills/skills.component';
+import { SchollComponent } from './components/landing/scholl/scholl.component';
+import { JobComponent } from './components/landing/job/job.component';
+import { PortfolioComponent } from './components/landing/portfolio/portfolio.component';
+import { InterestComponent } from './components/landing/interest/interest.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    MenuComponent,
+    SkillsComponent,
+    SchollComponent,
+    JobComponent,
+    PortfolioComponent,
+    InterestComponent
   ],
   imports: [
     BrowserModule
